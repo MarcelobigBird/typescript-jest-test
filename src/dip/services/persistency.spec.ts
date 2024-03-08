955b4bdf297ec82de2a868903b34584026dd04b8
MarcelobigBird <marcelomuckeiro2022@gmail.com>
2024-03-08 20:18:19 -0300

    Primeiro teste com spyOn

--- src/dip/services/persistency.spec.ts
+++ src/dip/services/persistency.spec.ts
@@ -1,13 +1,18 @@
-describe('Testando alguma coisa', () => {
-  it('should return one', () => {
-    const number = 1;
-    expect(number).toBe(1);
-  });
-});
+import { Persistency } from './persistency';
 
-describe('Testando outra coisa', () => {
-  test('should return Luiz', () => {
-    const name = 'Luiz';
-    expect(name).toBe('Luiz');
+describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    // Classe ou sistema que está sendo testado
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('should call console.log with "Pedido salvo com sucesso..."', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
   });
 });
