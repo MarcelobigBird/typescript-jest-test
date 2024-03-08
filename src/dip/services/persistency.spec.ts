a97bdbf143ba92f65d0d19bd82422ffd81462957
MarcelobigBird <marcelomuckeiro2022@gmail.com>
2024-03-07 22:16:24 -0300

    Asserções com jest

--- src/dip/services/persistency.spec.ts
+++ src/dip/services/persistency.spec.ts
@@ -1,12 +1,12 @@
 describe('Testando alguma coisa', () => {
-  it('test description (IT)', () => {
+  it('should return one', () => {
     const number = 1;
     expect(number).toBe(1);
   });
 });
 
 describe('Testando outra coisa', () => {
-  test('test description (TEST)', () => {
+  test('should return Luiz', () => {
     const name = 'Luiz';
     expect(name).toBe('Luiz');
   });
