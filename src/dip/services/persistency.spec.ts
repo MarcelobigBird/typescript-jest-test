3d09c21cd5b08cbd488f33209796896df0943a20
MarcelobigBird <marcelomuckeiro2022@gmail.com>
2024-03-09 12:03:52 -0300

    Primeiro teste com spyOn correção

--- src/dip/services/persistency.spec.ts
+++ src/dip/services/persistency.spec.ts
@@ -9,6 +9,13 @@ describe('Persistency', () => {
     expect(sut.saveOrder()).toBeUndefined();
   });
 
+  it('should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
   it('should call console.log with "Pedido salvo com sucesso..."', () => {
     const sut = new Persistency();
     const consoleSpy = jest.spyOn(console, 'log');
